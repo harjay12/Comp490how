d62f9054b9f06f395270467387ef4e1f1efc754b
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-04-27 23:10:31 -0400

    Test for negative numbers

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pytest
+
 file_path = os.path.abspath(__file__)
 cur_path = os.path.dirname(file_path)
 project_path = os.path.dirname(cur_path)
@@ -15,23 +16,31 @@ def main():
 
 def test_ma_final_price(main):
     state = 'MA'
-    a = [{'State': state, 'price': 175, 'type': 'Clothing'},
-         {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
-         {'State': state, 'price': 6, 'type': 'everything else'}]
-    assert main.final_price(state, a) == 701.3125
+    records = [{'State': state, 'price': 175, 'type': 'Clothing'},
+               {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
+               {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, records) == 701.3125
 
 
 def test_me_final_price(main):
     state = 'ME'
-    a = [{'State': state, 'price': 175, 'type': 'Clothing'},
-         {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
-         {'State': state, 'price': 6, 'type': 'everything else'}]
-    assert main.final_price(state, a) == 718.455
+    records = [{'State': state, 'price': 175, 'type': 'Clothing'},
+               {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
+               {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, records) == 718.455
 
 
 def test_all_and_nh_final_price(main):
     state = 'NH'
-    a = [{'State': state, 'price': 175, 'type': 'Clothing'},
-         {'State': state, 'price': 50, 'type': 'Wic Eligible food'},
-         {'State': state, 'price': 6, 'type': 'everything else'}]
-    assert main.final_price(state, a) == (175 + 50 + 6)
+    records = [{'State': state, 'price': 175, 'type': 'Clothing'},
+               {'State': state, 'price': 50, 'type': 'Wic Eligible food'},
+               {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, records) == (175 + 50 + 6)
+
+
+def test_no_refund_final_price(main):
+    state = 'NH'
+    records = [{'State': state, 'price': 175, 'type': 'Clothing'},
+               {'State': state, 'price': -500, 'type': 'Wic Eligible food'},
+               {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, records) == 'No Returned allowed'
