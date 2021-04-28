074042c827e85bc9ce16e625c6e09c432c23b37b
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-04-27 22:33:48 -0400

    Updated test folder

---
+++ tests/test.py
@@ -0,0 +1,25 @@
+import main
+
+
+def test_ma_final_price():
+    state = 'MA'
+    a = [{'State': state, 'price': 175, 'type': 'Clothing'},
+         {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
+         {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, a) == 701.3125
+
+
+def test_me_final_price():
+    state = 'ME'
+    a = [{'State': state, 'price': 175, 'type': 'Clothing'},
+         {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
+         {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, a) == 718.455
+
+
+def test_all_and_nh_final_price():
+    state = 'NH'
+    a = [{'State': state, 'price': 175, 'type': 'Clothing'},
+         {'State': state, 'price': 50, 'type': 'Wic Eligible food'},
+         {'State': state, 'price': 6, 'type': 'everything else'}]
+    assert main.final_price(state, a) == (175+50+6)
