c727cc4224527cf921a8ae107dce888dfef8dc45
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-04-27 22:55:15 -0400

    Still fixing git action issue

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,19 @@
 import os
 import sys
-import main
-
+import pytest
 file_path = os.path.abspath(__file__)
 cur_path = os.path.dirname(file_path)
 project_path = os.path.dirname(cur_path)
 sys.path.append(project_path)
 
 
-def test_ma_final_price():
+@pytest.fixture
+def main():
+    import main
+    return main
+
+
+def test_ma_final_price(main):
     state = 'MA'
     a = [{'State': state, 'price': 175, 'type': 'Clothing'},
          {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
@@ -16,7 +21,7 @@ def test_ma_final_price():
     assert main.final_price(state, a) == 701.3125
 
 
-def test_me_final_price():
+def test_me_final_price(main):
     state = 'ME'
     a = [{'State': state, 'price': 175, 'type': 'Clothing'},
          {'State': state, 'price': 500, 'type': 'Wic Eligible food'},
@@ -24,7 +29,7 @@ def test_me_final_price():
     assert main.final_price(state, a) == 718.455
 
 
-def test_all_and_nh_final_price():
+def test_all_and_nh_final_price(main):
     state = 'NH'
     a = [{'State': state, 'price': 175, 'type': 'Clothing'},
          {'State': state, 'price': 50, 'type': 'Wic Eligible food'},
